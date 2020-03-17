fb5720e8e1a7307252da672fbdf43f72a53dd0c7
Emil Gafiyatullin <emilyandro99@gmail.com>
2020-03-17 10:33:48 +0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+import Modules.App
+import Modules.Data
+import Modules.Device
+import Modules.Domain
+
+include(App)
+include(Domain)
+include(Data)
+include(Device)
+rootProject.name="AndroidAppTemplate"
